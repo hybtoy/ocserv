ee6285547ef1ecabda843093d585479cf5ac1dca
hybtoy <hybtoy@gmail.com>
2020-04-21 18:23:22 +0500

    Update install_script.sh

--- install_script.sh
+++ install_script.sh
@@ -150,6 +150,7 @@ iptables -F
 iptables -A INPUT -i lo -j ACCEPT
 iptables -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
 iptables -A INPUT -p icmp -j ACCEPT
+iptables -I INPUT -p tcp --dport 22 -j ACCEPT
 iptables -I INPUT -p tcp --dport 80 -j ACCEPT
 iptables -A INPUT -p tcp --dport 443 -j ACCEPT
 iptables -A INPUT -p tcp --dport 22227 -j ACCEPT
